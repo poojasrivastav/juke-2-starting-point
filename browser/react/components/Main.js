6c941f417b9314895f29513205f7a7569ab24fea
Jason <Jason@Jasons-MacBook-Pro-2.local>
2017-08-21 18:12:15 -0400

    second commit

--- browser/react/components/Main.js
+++ browser/react/components/Main.js
@@ -4,34 +4,35 @@ import AllAlbums from './AllAlbums';
 import SingleAlbum from './SingleAlbum';
 import Sidebar from './Sidebar';
 import Player from './Player';
+import {HashRouter, Route} from 'react-router-dom';
 
 export default class Main extends Component {
 
   constructor (props) {
     super(props);
     this.state = {
-      albums: [],
+      // albums: [],
       selectedAlbum: {}
     };
-    this.selectAlbum = this.selectAlbum.bind(this);
+    // this.selectAlbum = this.selectAlbum.bind(this);
     this.deselectAlbum = this.deselectAlbum.bind(this);
   }
 
-  componentDidMount () {
-    axios.get('/api/albums/')
-      .then(res => res.data)
-      .then(albums => {
-        this.setState({ albums })
-      });
-  }
+  // componentDidMount () {
+  //   axios.get('/api/albums/')
+  //     .then(res => res.data)
+  //     .then(albums => {
+  //       this.setState({ albums })
+  //     });
+  // }
 
-  selectAlbum (albumId) {
-    axios.get(`/api/albums/${albumId}`)
-      .then(res => res.data)
-      .then(album => this.setState({
-        selectedAlbum: album
-      }));
-  }
+  // selectAlbum (albumId) {
+  //   axios.get(`/api/albums/${albumId}`)
+  //     .then(res => res.data)
+  //     .then(album => this.setState({
+  //       selectedAlbum: album
+  //     }));
+  // }
 
   deselectAlbum () {
     this.setState({ selectedAlbum: {}});
@@ -43,13 +44,12 @@ export default class Main extends Component {
         <div className="col-xs-2">
           <Sidebar deselectAlbum={this.deselectAlbum} />
         </div>
+        <HashRouter>
         <div className="col-xs-10">
-        {
-          this.state.selectedAlbum.id ?
-          <SingleAlbum album={this.state.selectedAlbum} /> :
-          <AllAlbums albums={this.state.albums} selectAlbum={this.selectAlbum} />
-        }
+        <Route exact path= '/' component={AllAlbums}/>
+        <Route path= '/albums' component={AllAlbums}/>
         </div>
+        </HashRouter>
         <Player />
       </div>
     );
